c53b1c9bdc603063564ab7aff4493c4990075df4
Alan <alan@roojs.com>
2025-11-20 15:02:00 +0800

    convert to a tool that can be used with tree-sitter  - remove tree sitter library

--- setup.py
+++ setup.py
@@ -1,46 +1,40 @@
-import setuptools
+from setuptools import setup, find_packages
 
-with open('README.md', 'r') as f:
-    long_description = f.read()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-setuptools.setup(
-    name='semantic-code-search',
-    version='0.4.0',
-    author='Kiril Videlov',
-    author_email='kiril@codeball.ai',
-    description='Search your codebase with natural language.',
-    install_requires=[
-                'numpy==1.22.4',
-                'prompt_toolkit==3.0.32',
-                'Pygments==2.12.0',
-                'sentence_transformers==2.2.2',
-                'setuptools==62.6.0',
-                'torch==1.12.1',
-                'tree_sitter==0.20.1',
-                'tree_sitter_builds==2022.8.27',
-                'tree_sitter_languages==1.5.0',
-    ],
+setup(
+    name="semantic-code-search",
+    version="0.4.0",
+    author="Original Author",
+    author_email="kiril@codeball.ai",
+    description="Search your codebase with natural language",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    python_requires='>=3.8',
-    url='https://github.com/sturdy-dev/semantic-code-search',
-    package_dir={
-        "semantic_code_search": "src/semantic_code_search"
-    },
-    entry_points={
-        'console_scripts': [
-            'sem=semantic_code_search.cli:main',
-        ]
-    },
-    keywords='semantic code search',
+    long_description_content_type="text/markdown",
+    url="https://github.com/sturdy-dev/semantic-code-search",
+    packages=find_packages(where="src"),  # Look in src directory
+    package_dir={"": "src"},              # Map root package to src
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Affero General Public License v3',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Topic :: Software Development',
-        'Topic :: Utilities',
-    ]
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "numpy>=1.24.0",
+        "prompt_toolkit>=3.0.39",
+        "Pygments>=2.15.0",
+        "sentence_transformers>=2.2.2",
+        "torch>=2.0.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "sem=semantic_code_search.cli:main",
+        ],
+    },
 )
